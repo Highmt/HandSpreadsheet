ea77fa8117576a0e93fdbfc1152124f2ef28267e
Highmt <yprogdbcs@icloud.com>
2021-12-10 15:36:10 +0900

    reformat

--- src/UDP/PythonSample.py
+++ src/UDP/PythonSample.py
@@ -26,22 +26,13 @@ from src.UDP import MoCapData
 
 # This is a callback function that gets connected to the NatNet client
 # and called once per mocap frame.
-def receive_new_frame(data_dict):
-    order_list=[ "frameNumber", "markerSetCount", "unlabeledMarkersCount", "rigidBodyCount", "skeletonCount",
-                "labeledMarkerCount", "timecode", "timecodeSub", "timestamp", "isRecording", "trackedModelsChanged" ]
-    dump_args = False
-    if dump_args == True:
-        out_string = "    "
-        for key in data_dict:
-            out_string += key + "="
-            if key in data_dict :
-                out_string += data_dict[key] + " "
-            out_string+="/"
-        print(out_string)
+def receive_new_frame(mocap_data=MoCapData.MoCapData()):
+    print("-----------new Frame----------")
+
 
 # This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
 def receive_rigid_body_frame( new_id, position, rotation ):
-    pass
+    print("---------------Rigid Body---------------------------")
     #print( "Received frame for rigid body", new_id )
     #print( "Received frame for rigid body", new_id," ",position," ",rotation )
 
@@ -149,6 +140,115 @@ def my_parse_args(arg_list, args_dict):
     return args_dict
 
 
+def print_message(streaming_client):
+    is_looping = True
+    if streaming_client.connected() is False:
+        print("ERROR: Could not connect properly.  Check that Motive streaming is on.")
+        try:
+            sys.exit(2)
+        except SystemExit:
+            print("...")
+        finally:
+            print("exiting")
+    print_configuration(streaming_client)
+    print("\n")
+    print_commands(streaming_client.can_change_bitstream_version())
+    while is_looping:
+        inchars = input('Enter command or (\'h\' for list of commands)\n')
+        if len(inchars) > 0:
+            c1 = inchars[0].lower()
+            if c1 == 'h':
+                print_commands(streaming_client.can_change_bitstream_version())
+            elif c1 == 'c':
+                print_configuration(streaming_client)
+            elif c1 == 's':
+                request_data_descriptions(streaming_client)
+                time.sleep(1)
+            elif (c1 == '3') or (c1 == '4'):
+                if streaming_client.can_change_bitstream_version():
+                    tmp_major = 4
+                    tmp_minor = 0
+                    if (c1 == '3'):
+                        tmp_major = 3
+                        tmp_minor = 1
+                    return_code = streaming_client.set_nat_net_version(tmp_major, tmp_minor)
+                    time.sleep(1)
+                    if return_code == -1:
+                        print("Could not change bitstream version to %d.%d" % (tmp_major, tmp_minor))
+                    else:
+                        print("Bitstream version at %d.%d" % (tmp_major, tmp_minor))
+                else:
+                    print("Can only change bitstream in Unicast Mode")
+
+            elif c1 == 'p':
+                sz_command = "TimelineStop"
+                return_code = streaming_client.send_command(sz_command)
+                time.sleep(1)
+                print("Command: %s - return_code: %d" % (sz_command, return_code))
+            elif c1 == 'r':
+                sz_command = "TimelinePlay"
+                return_code = streaming_client.send_command(sz_command)
+                print("Command: %s - return_code: %d" % (sz_command, return_code))
+            elif c1 == 'o':
+                tmpCommands = ["TimelinePlay",
+                               "TimelineStop",
+                               "SetPlaybackStartFrame,0",
+                               "SetPlaybackStopFrame,1000000",
+                               "SetPlaybackLooping,0",
+                               "SetPlaybackCurrentFrame,0",
+                               "TimelineStop"]
+                for sz_command in tmpCommands:
+                    return_code = streaming_client.send_command(sz_command)
+                    print("Command: %s - return_code: %d" % (sz_command, return_code))
+                time.sleep(1)
+            elif c1 == 'w':
+                tmp_commands = ["TimelinePlay",
+                                "TimelineStop",
+                                "SetPlaybackStartFrame,10",
+                                "SetPlaybackStopFrame,1500",
+                                "SetPlaybackLooping,0",
+                                "SetPlaybackCurrentFrame,100",
+                                "TimelineStop"]
+                for sz_command in tmp_commands:
+                    return_code = streaming_client.send_command(sz_command)
+                    print("Command: %s - return_code: %d" % (sz_command, return_code))
+                time.sleep(1)
+            elif c1 == 't':
+                test_classes()
+
+            elif c1 == 'j':
+                streaming_client.set_print_level(0)
+                print("Showing only received frame numbers and supressing data descriptions")
+            elif c1 == 'k':
+                streaming_client.set_print_level(1)
+                print("Showing every received frame")
+
+            elif c1 == 'l':
+                print_level = streaming_client.set_print_level(20)
+                print_level_mod = print_level % 100
+                if (print_level == 0):
+                    print("Showing only received frame numbers and supressing data descriptions")
+                elif (print_level == 1):
+                    print("Showing every frame")
+                elif (print_level_mod == 1):
+                    print("Showing every %dst frame" % print_level)
+                elif (print_level_mod == 2):
+                    print("Showing every %dnd frame" % print_level)
+                elif (print_level == 3):
+                    print("Showing every %drd frame" % print_level)
+                else:
+                    print("Showing every %dth frame" % print_level)
+
+            elif c1 == 'q':
+                is_looping = False
+                streaming_client.shutdown()
+                break
+            else:
+                print("Error: Command %s not recognized" % c1)
+            print("Ready...\n")
+    print("exiting")
+
+
 if __name__ == "__main__":
 
     optionsDict = {}
@@ -180,113 +280,5 @@ if __name__ == "__main__":
         finally:
             print("exiting")
 
-    is_looping = True
     time.sleep(1)
-    if streaming_client.connected() is False:
-        print("ERROR: Could not connect properly.  Check that Motive streaming is on.")
-        try:
-            sys.exit(2)
-        except SystemExit:
-            print("...")
-        finally:
-            print("exiting")
-
-    print_configuration(streaming_client)
-    print("\n")
-    print_commands(streaming_client.can_change_bitstream_version())
-
-
-    while is_looping:
-        inchars = input('Enter command or (\'h\' for list of commands)\n')
-        if len(inchars)>0:
-            c1 = inchars[0].lower()
-            if c1 == 'h' :
-                print_commands(streaming_client.can_change_bitstream_version())
-            elif c1 == 'c' :
-                print_configuration(streaming_client)
-            elif c1 == 's':
-                request_data_descriptions(streaming_client)
-                time.sleep(1)
-            elif (c1 == '3') or (c1 == '4'):
-                if streaming_client.can_change_bitstream_version():
-                    tmp_major = 4
-                    tmp_minor = 0
-                    if(c1 == '3'):
-                        tmp_major = 3
-                        tmp_minor = 1
-                    return_code = streaming_client.set_nat_net_version(tmp_major,tmp_minor)
-                    time.sleep(1)
-                    if return_code == -1:
-                        print("Could not change bitstream version to %d.%d"%(tmp_major,tmp_minor))
-                    else:
-                        print("Bitstream version at %d.%d"%(tmp_major,tmp_minor))
-                else:
-                    print("Can only change bitstream in Unicast Mode")
-
-            elif c1 == 'p':
-                sz_command="TimelineStop"
-                return_code = streaming_client.send_command(sz_command)
-                time.sleep(1)
-                print("Command: %s - return_code: %d"% (sz_command, return_code) )
-            elif c1 == 'r':
-                sz_command="TimelinePlay"
-                return_code = streaming_client.send_command(sz_command)
-                print("Command: %s - return_code: %d"% (sz_command, return_code) )
-            elif c1 == 'o':
-                tmpCommands=["TimelinePlay",
-                            "TimelineStop",
-                            "SetPlaybackStartFrame,0",
-                            "SetPlaybackStopFrame,1000000",
-                            "SetPlaybackLooping,0",
-                            "SetPlaybackCurrentFrame,0",
-                            "TimelineStop"]
-                for sz_command in tmpCommands:
-                    return_code = streaming_client.send_command(sz_command)
-                    print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                time.sleep(1)
-            elif c1 == 'w':
-                tmp_commands=["TimelinePlay",
-                            "TimelineStop",
-                            "SetPlaybackStartFrame,10",
-                            "SetPlaybackStopFrame,1500",
-                            "SetPlaybackLooping,0",
-                            "SetPlaybackCurrentFrame,100",
-                            "TimelineStop"]
-                for sz_command in tmp_commands:
-                    return_code = streaming_client.send_command(sz_command)
-                    print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                time.sleep(1)
-            elif c1 == 't':
-                test_classes()
-
-            elif c1 == 'j':
-                streaming_client.set_print_level(0)
-                print("Showing only received frame numbers and supressing data descriptions")
-            elif c1 == 'k':
-                streaming_client.set_print_level(1)
-                print("Showing every received frame")
-
-            elif c1 == 'l':
-                print_level = streaming_client.set_print_level(20)
-                print_level_mod = print_level % 100
-                if(print_level == 0):
-                    print("Showing only received frame numbers and supressing data descriptions")
-                elif (print_level == 1):
-                    print("Showing every frame")
-                elif (print_level_mod == 1):
-                    print("Showing every %dst frame"%print_level)
-                elif (print_level_mod == 2):
-                    print("Showing every %dnd frame"%print_level)
-                elif (print_level == 3):
-                    print("Showing every %drd frame"%print_level)
-                else:
-                    print("Showing every %dth frame"%print_level)
-
-            elif c1 == 'q':
-                is_looping = False
-                streaming_client.shutdown()
-                break
-            else:
-                print("Error: Command %s not recognized"%c1)
-            print("Ready...\n")
-    print("exiting")
+    print_message(streaming_client)
