da7884716345eeb393d6685a411fdeeb60e2eef9
Highmt <yprogdbcs@icloud.com>
2021-12-13 21:48:49 +0900

    fix feature list to Enum TODO: convert dataframe to pandas.Series

--- src/HandScensing/Predictor.py
+++ src/HandScensing/Predictor.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 
 from lib.LeapMotion.Leap import RAD_TO_DEG
+from res.SSEnum import FeatureEnum
 from src.HandScensing.HandListener import HandData
 
 model_pass = '../../res/learningModel/'
@@ -18,22 +19,21 @@ class Predictor():
         self.df = self.create_emptypandas()
         self.dfs = self.create_emptypandas()
 
-    def handPredict(self, hand: HandData, isLeft: bool):
+    def handPredict(self, hand: HandData):
 
         for finger_id in len(hand.fingers_pos):
-                if finger_id == 0:
-                    for i in range(3):
-                        self.df.at[0, self.finger_labels[finger_id] + "_pos_" + self.pos_labels[i]] = hand.fingers_pos[i]
-                        self.df.at[0, self.finger_labels[finger_id] + "dir" + self.pos_labels[i]] = hand.fingers_pos[i] - hand.position[i]
+            for pos in range(3):
+                self.df.at[0, self.finger_labels[finger_id] + "_pos_" + self.pos_labels[pos]] = hand.fingers_pos[pos]
+                self.df.at[0, self.finger_labels[finger_id] + "dir" + self.pos_labels[pos]] = hand.fingers_pos[pos] - hand.position[pos]
 
-        for i in range(3):
-            self.df.at[0, self.pos_labels[i]] = hand.position[i]
-            self.df.at[0, self.rot_labels[i]] = hand.rotation[i]
+        for pos in range(3):
+            self.df.at[0, self.pos_labels[pos]] = hand.position[pos]
+            self.df.at[0, self.rot_labels[pos]] = hand.rotation[pos]
 
 
         self.dfs = pd.concat([self.dfs, self.df], ignore_index=True)
 
-        if isLeft:
+        if hand.is_left:
             pred = self.left_model.predict(self.df.values)
         else:
             pred = self.right_model.predict(self.df.values)
@@ -42,29 +42,4 @@ class Predictor():
         return pred[0]
 
     def create_emptypandas(self):
-        return pd.DataFrame(index=[0], columns=[
-            "position_x",
-            "position_y",
-            "position_z",
-            "pitch",
-            "roll",
-            "yaw",
-            "Thumb_pos_x",
-            "Thumb_pos_y",
-            "Thumb_pos_z",
-            "Index_pos_x",
-            "Index_pos_y",
-            "Index_pos_z",
-            "Pinky_pos_x",
-            "Pinky_pos_y",
-            "Pinky_pos_z",
-            "Thumb_dir_x",
-            "Thumb_dir_y",
-            "Thumb_dir_z",
-            "Index_dir_x",
-            "Index_dir_y",
-            "Index_dir_z",
-            "Pinky_dir_x",
-            "Pinky_dir_y",
-            "Pinky_dir_z",
-        ])
+        return pd.DataFrame(columns=FeatureEnum.FEATURE_LIST.value)
