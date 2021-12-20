23232f52f21dc9c4bc4516f84d33df1fe3bb7f74
Highmt <yprogdbcs@icloud.com>
2021-12-20 15:03:19 +0900

    complete real time function

--- src/HandScensing/RealtimePlot.py
+++ src/HandScensing/RealtimePlot.py
@@ -41,21 +41,35 @@ def live_plotter(x_vec, y1_data, line1, identifier='', pause_time=0.001):
     return line1
 
 x_vec = np.linspace(0, 1, 100 + 1)[0:-1]
-y_vec = {
+l_y_vec = {
     'x': np.random.randn(len(x_vec)),
     'y': np.random.randn(len(x_vec)),
     'z': np.random.randn(len(x_vec))
 }
 line1 = {'x': [], 'y': [], 'z': []}
 
+r_y_vec = {
+    'x': np.random.randn(len(x_vec)),
+    'y': np.random.randn(len(x_vec)),
+    'z': np.random.randn(len(x_vec))
+}
+line2 = {'x': [], 'y': [], 'z': []}
+
 plt.ion()
-fig = plt.figure(figsize=(13, 6))
-ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(10, 8))
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
 # create a variable for the line so we can later update it
-line1['x'], = ax.plot(x_vec, y_vec['x'], '-', alpha=0.8)
-line1['y'], = ax.plot(x_vec, y_vec['y'], '-o', alpha=0.8)
-line1['z'], = ax.plot(x_vec, y_vec['z'], '--', alpha=0.8)
-plt.ylim([-3000, 3000])
+line1['x'], = ax1.plot(x_vec, l_y_vec['x'], '-', alpha=0.8)
+line1['y'], = ax1.plot(x_vec, l_y_vec['y'], '-o', alpha=0.8)
+line1['z'], = ax1.plot(x_vec, l_y_vec['z'], '--', alpha=0.8)
+
+
+line2['x'], = ax2.plot(x_vec, r_y_vec['x'], '-', alpha=0.8)
+line2['y'], = ax2.plot(x_vec, r_y_vec['y'], '-o', alpha=0.8)
+line2['z'], = ax2.plot(x_vec, r_y_vec['z'], '--', alpha=0.8)
+ax1.set_ylim(ymax=1000, ymin=-1000)
+ax2.set_ylim(ymax=1000, ymin=-1000)
 # update plot label/title
 plt.ylabel('Y Label')
 plt.title('Title: {}'.format('Realtime'))
@@ -74,17 +88,27 @@ sys.stdin.readline()
 listener.streaming_client.restart()
 try:
     while True:
-        mocap_data = listener.getCurrentData()
+        mocap_data: MoCapData = listener.getCurrentData()
         if listener.judgeDataComplete(mocap_data=mocap_data):
             listener.setHandData(mocap_data=mocap_data)
             for hand in listener.hands_dict.values():
-                y_vec['x'][-1] = hand.position[0]
-                y_vec['y'][-1] = hand.position[1]
-                y_vec['z'][-1] = hand.position[2]
-                line1 = live_plotter(x_vec, y_vec, line1)
-                y_vec['x'] = np.append(y_vec['x'][1:], 0.0)
-                y_vec['y'] = np.append(y_vec['y'][1:], 0.0)
-                y_vec['z'] = np.append(y_vec['z'][1:], 0.0)
+                listener.printHandData(hand)
+                if hand.is_left:
+                    l_y_vec['x'][-1] = hand.position[0]
+                    l_y_vec['y'][-1] = hand.position[1]
+                    l_y_vec['z'][-1] = hand.position[2]
+                    line1 = live_plotter(x_vec, l_y_vec, line1)
+                    l_y_vec['x'] = np.append(l_y_vec['x'][1:], 0.0)
+                    l_y_vec['y'] = np.append(l_y_vec['y'][1:], 0.0)
+                    l_y_vec['z'] = np.append(l_y_vec['z'][1:], 0.0)
+                else:
+                    r_y_vec['x'][-1] = hand.position[0]
+                    r_y_vec['y'][-1] = hand.position[1]
+                    r_y_vec['z'][-1] = hand.position[2]
+                    line2 = live_plotter(x_vec, r_y_vec, line2)
+                    r_y_vec['x'] = np.append(r_y_vec['x'][1:], 0.0)
+                    r_y_vec['y'] = np.append(r_y_vec['y'][1:], 0.0)
+                    r_y_vec['z'] = np.append(r_y_vec['z'][1:], 0.0)
             time.sleep(0.05)
 except KeyboardInterrupt:
     print("\n\nexit...")
