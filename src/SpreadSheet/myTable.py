53e6537b955a4a6ccbd4fc84d52fd47adbfc0be0
Highmt <yprogdbcs@icloud.com>
2021-12-31 19:39:42 +0900

    test に対応

--- src/SpreadSheet/myTable.py
+++ src/SpreadSheet/myTable.py
@@ -1,5 +1,7 @@
+import random
+
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QTableWidgetSelectionRange, QMenu, QAction
 
@@ -25,6 +27,13 @@ class myTable(QTableWidget):
         self.verticalHeader().setDefaultSectionSize(60)
         self.horizontalHeader().setDefaultSectionSize(120)
 
+        # for test
+        self.pre_target = QRect(0, 0, 0, 0)
+        self.target_top = 0
+        self.target_left = 0
+        self.target_height = 0
+        self.target_width = 0
+
     def setupHeader(self, cols):
         for c in range(cols):
             character = chr(ord('A') + c)
@@ -36,7 +45,7 @@ class myTable(QTableWidget):
                 self.setItem(i, j, SpreadSheetItem())
 
     def setupContents(self):
-        titleBackground = QColor(Qt.white)
+        titleBackground = QColor(Qt.lightGray)
         titleFont = self.font()
         titleFont.setBold(True)
         # column 0
@@ -125,7 +134,7 @@ class myTable(QTableWidget):
 
                 for j in range(selectrange.leftColumn(), selectrange.rightColumn() + 1):
                     self.setItem(i, j, SpreadSheetItem())
-                    self.item(i, j).setBackground(Qt.red)
+                    # self.item(i, j).setBackground(Qt.red)
         else:
             for j in range(selectrange.leftColumn(), selectrange.rightColumn() + 1):
 
@@ -134,7 +143,7 @@ class myTable(QTableWidget):
                     self.setItem(i, j, temp)
                 for i in range(selectrange.topRow(), selectrange.bottomRow() + 1):
                     self.setItem(i, j, SpreadSheetItem())
-                    self.item(i, j).setBackground(Qt.red)
+                    # self.item(i, j).setBackground(Qt.red)
 
     def deleteCell(self, d):
         selectrange = self.selectedRanges()[0]
@@ -232,3 +241,27 @@ class myTable(QTableWidget):
             return self.visualItemRect(itemList[0]), self.visualItemRect(itemList[-1])
             # self.first_item = itemList[0]
             # self.last_item = itemList[-1]
+
+    def setRandomCellColor(self):
+        while True:
+            self.target_top = random.randint(1, self.columnCount()-4)
+            self.target_left = random.randint(1, self.rowCount()-4)
+            self.target_height = random.randint(1, 3)
+            self.target_width = random.randint(1, 3)
+            if self.target_top < self.pre_target.y() or \
+                    self.target_left < self.pre_target.x() or \
+                    self.target_height + self.target_top > self.pre_target.height() + self.pre_target.y() or \
+                    self.target_width + self.target_left > self.pre_target.width() + self.pre_target.x():
+                break
+
+        for i in range(self.target_height):
+            for j in range(self.target_width):
+                self.item(self.target_top + i, self.target_left + j).setBackground(Qt.blue)
+
+        self.pre_target = QRect(self.target_left, self.target_top, self.target_width, self.target_height)
+
+
+    def resetRandomCellColor(self):
+        for i in range(self.target_height):
+            for j in range(self.target_width):
+                self.item(self.target_top + i, self.target_left + j).setBackground(Qt.white)
