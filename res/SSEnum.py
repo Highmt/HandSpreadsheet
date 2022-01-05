a02daa409bb5c960ea199ede35df46061c616ae4
Highmt <yprogdbcs@icloud.com>
2022-01-05 16:01:55 +0900

    add access by Operation and Direction

--- res/SSEnum.py
+++ res/SSEnum.py
@@ -8,7 +8,6 @@ class DirectionEnum(Enum):
     BACK = 3
     NONE = 4
 
-
 class HandEnum(Enum):
     FREE = 0
     PINCH_IN = 1
@@ -28,8 +27,16 @@ class OperationEnum(Enum):
     Paste = 6
     Sa = 7
     Sd = 8
+    OperationGrid = [
+        [0, 1, None, None, None],
+        [2, 3, None, None, None],
+        [None, None, None, None, 4],
+        [None, None, None, None, 5],
+        [None, None, None, None, 6],
+        [None, None, 7, 8, None]
+    ]
     OperationName_LIST = ["Ir", "Id", "Dl", "Du", "Copy", "Cut", "Paste", "Sa", "Sd"]
-    OperationName_LIST_JP = ["列の挿入", "行の挿入", "烈の削除", "行の削除", "コピー", "カット", "ペースト", "昇順ソート", "降順ソート"]
+    OperationName_LIST_JP = ["列の挿入", "行の挿入", "列の削除", "行の削除", "コピー", "カット", "ペースト", "昇順ソート", "降順ソート"]
 
 
 
